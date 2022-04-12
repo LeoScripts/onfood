8c0fad4046fd45081ecbc19cc461fc90d5b37b49
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-11 21:49:52 -0300

    developmente initial components

---
+++ components/header/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <footer>
+      <h1>Header</h1>
+    </footer>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Header } from "../components"
+
 export default function Home() {
   return (
+    <div>
+
+    <Header />
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </div>
   )
 }
