c81677d8d7c1dbd364d72d89d06fc1d2902574de
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-13 22:04:45 -0300

    feat:styled header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Styles from './header.module.css';
+
 
 export function Header() {
   return (
     <header>
       <nav className={Styles.nav}>
-        <div className={Styles.brand}>[logo]</div>
+        <div className={Styles.brand}>
+          <Image src="/images/logo.png" width={45} height={36} alt="OnFood" />
+        </div>
         <ul>
           <li>
             <Link href="/">
