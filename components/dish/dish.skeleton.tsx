8ab6e33ebbbd80bc0b6c178796f72536e35eb3f4
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-04-22 20:57:13 -0300

    chere: styded skeleton

--- components/dish/dish.skeleton.tsx
+++ components/dish/dish.skeleton.tsx
@@ -5,8 +5,35 @@ export function DishSkeleton() {
   return (
     <SkeletonTheme baseColor="#ccc" highlightColor="#eee">
       <div>
-        <Skeleton  count={1} height={200} width="100%" style={{ marginBottom: "20px" }}/>
+        <Skeleton  
+          count={1} 
+          height={200} 
+          width="100%" 
+          style={
+            { 
+              marginBottom: "5px" 
+            }
+          }
+        />
+        <Skeleton inline={true} height={24} width="40%" count={1} />
+        <Skeleton 
+          inline={true} 
+          height={24} 
+          width="20%" 
+          count={1} 
+          style={
+            {
+              marginLeft: "40%"
+            }
+          } 
+        />
+      <div className="relative mb-12">
+
+      <Skeleton inline={true} height={24} width="50%" count={1} />
       </div>
+      </div>
+
+      
     </SkeletonTheme>
   )
 }
